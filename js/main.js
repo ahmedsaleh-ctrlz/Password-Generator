f41658ddcdb97cbb0bf1f219f3210a78549fde36
Ahmed <ahmedsalehx11@gmail.com>
2026-01-09 10:37:55 +0200

    Modified js and Update Css File and html File

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// global variables
+
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-=[]{}|;:',.<>?";
+const allChars = upper + lower + numbers + symbols;
+
+const btn = getElement("btn");
+const input = getElement("input");
+const Copy = getElement("Copy");
+
+// Utilty Func
+function getElement(id) {
+  return document.getElementById(id);
+}
+
+function GeneratePassword(length = 12) {
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+
+  return password;
+}
+
+function showAlert() {
+  let alert = document.querySelector(".alert-container");
+  alert.classList.remove("hide");
+  setTimeout(() => {
+    alert.classList.add("hide");
+  }, 3000);
+}
+
+function playSound() {
+  let sound = new Audio("/audio/success.mp3");
+  sound.play();
+}
+
+function ChangeCopyText(text) {
+  Copy.firstChild.textContent = text;
+}
+
+async function copyPasswordToClipboard() {
+    if(!input.value) return false;
+
+    try{
+        await navigator.clipboard.writeText(input.value);
+        return true;
+    }
+    catch{
+        return false
+    }
+}
+
+//#region GeneratePassword
+
+btn.addEventListener("click", () => {
+  input.value = GeneratePassword();
+  showAlert();
+  playSound();
+});
+
+//#endregion
+
+// Copy Feauture
+
+Copy.addEventListener("click",async () => {
+  if (await copyPasswordToClipboard()) {
+    ChangeCopyText("Copied");
+  }
+  else{
+    ChangeCopyText("Empty Field");
+  }
+
+  setTimeout(()=>{ChangeCopyText("Copy")},1000);
+
+});
+
